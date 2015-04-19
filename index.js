bc54ad20d986722b9c83a4496fcb22742a52f835
Keely Hill <KeelyHill@gmx.com>
2015-04-19 10:45:37 -0400

    Removed old code and comments. Formatting. Add a few comments for code reference.

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-$(function () {
-
-});
-
 var src = $('#grid');
 var wrap = $('<div id="grid-overlay"></div>');
 var gsize = 7;
 
-var cols = 105; // 50
-var rows = 16; // 30 looks nice
+// these are 1 less due to counting from 0
+var cols = 105; // does not really matter
+var rows = 16; // matters alot.
 var gridArea = (cols + 1) * (rows + 1);
 
-// create overlay
+/* Overlay */
+
 var tbl = $('<table></table>');
 for (var y = rows; y >= 0; y--) {
     var tr = $("<tr id='tr" + y + "'></tr>");
@@ -23,12 +21,11 @@ for (var y = rows; y >= 0; y--) {
     tbl.append(tr);
 }
 
-// wrap.css('width', cols * (gsize + 2 * 2) + 'px').css('height', rows * (gsize + 2 * 2) + 'px');
-// src.css ('width', cols * (gsize + 2 * 2) + 'px').css('height', rows * (gsize + 2 * 2) + 'px');
-
 // attach overlay
 wrap.append(tbl);
 src.after(wrap);
+
+/* Formula functions */
 
 var bitString = "";
 var decimal = BigNumber;
@@ -63,14 +60,11 @@ var setBitMap = function() {
             if (bit == "0" || !bit) {
                 td.addClass('unselected');
                 td.removeClass('selected');
-                // console.log("[ ]");
             }
             if (bit == "1") {
                 td.addClass('selected');
-                td.removeClass('unselected');
-                // console.log('[X]');
-            }
-
+                td.removeClass('unselected');                
+	    }
             i++;
         }
     }
@@ -80,7 +74,6 @@ var getDecimalFromMap = function () {
     var bitString = "";
     for (var y = 0; y <= cols; y++) {
         for (var x = 0; x <= rows; x++) {
-
             var boolBit = $("table #tr" + x + " #td" + y).hasClass('selected') ? 1 : 0;
 
             bitString += boolBit;
@@ -90,9 +83,9 @@ var getDecimalFromMap = function () {
 
     setBitString(bitString);
     setDecString(decimal.times(17));
-
-    // $("#debug").append(decimal + " from " + bitString + "<br>");
 }
+
+/* Events */
 
 $('#grid-overlay td').hover(function () {
     $(this).toggleClass('hover');
@@ -162,3 +155,4 @@ $("#presets button").click(function() {
     setBitString("0".repeat(parseInt(b.attr('p'))) + big.dividedBy(17).floor().toString(2));
     setBitMap();
 });
+
